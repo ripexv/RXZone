dd5fc469e334df2c91b9f674c051aa4e842d01a0
gurkan <176038875+ripexv@users.noreply.github.com>
2026-08-14 15:35:12 +0300

    Initial Commit

---
+++ RXZone/RXZoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  RXZoneApp.swift
+//  RXZone
+//
+//  Created by Gürkan Genç on 14.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct RXZoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
